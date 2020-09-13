1bcabde583f1e23c5d98bed83b27894b684efe76
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-13 20:36:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,6 @@ def monopoly_with_third_tier
                    names: {}
       },
   }
-end
   
   monopoly[:railroads][:names] = reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}
   # When you start writing the implementation for this method, copy the Hash
