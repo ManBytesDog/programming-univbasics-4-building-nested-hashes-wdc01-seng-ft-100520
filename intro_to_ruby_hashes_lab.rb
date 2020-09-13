f1eafab52dd5cb5915a3f5b320e7b622056d8a4e
T.J. Marstiller <thomasj.marstiller@yahoo.com>
2020-09-13 20:43:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,6 @@ def monopoly_with_second_tier
                    names: {}
       },
   }
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
 
 end
 
@@ -32,10 +28,6 @@ def monopoly_with_third_tier
   }
   
   monopoly[:railroads][:names] = {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
   
   monopoly
   
